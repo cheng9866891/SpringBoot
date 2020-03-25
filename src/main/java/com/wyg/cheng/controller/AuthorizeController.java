707fecc6b0f8958c5e7d6418b39c5d0815615f7d
cheng <chengkeqiang123@163.com>
2020-03-25 15:30:59 +0800

    添加github登陆认证

--- src/main/java/com/wyg/cheng/controller/AuthorizeController.java
+++ src/main/java/com/wyg/cheng/controller/AuthorizeController.java
@@ -5,9 +5,12 @@ import com.wyg.cheng.dto.GithubUserDTO;
 import com.wyg.cheng.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class AuthorizeController {
@@ -21,9 +24,11 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name ="code") String code,
-                           @RequestParam(name="state")String state){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientID);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -31,11 +36,15 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUserDTO githubUser = githubProvider.getGithubUser(accessToken);
-        System.out.println(githubUser.getName());
-        return "index";
+        GithubUserDTO user = githubProvider.getGithubUser(accessToken);
+        System.out.println(user.getName());
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
-
 
 
 }
